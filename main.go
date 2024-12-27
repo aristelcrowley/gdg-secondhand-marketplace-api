4fd1e47736da9f9b5bf912bd8c6c9665a3ee5c9b
aristelcrowley <andhikapratamaunsri@gmail.com>
2024-12-27 19:00:43 +0700

    init

---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+	"os"
+	"secondhand-marketplace-api/config"
+	"secondhand-marketplace-api/routes"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	config.ConnectDatabase()
+
+	routes.SetupRoutes(app)
+
+	err = app.Listen(":3000")
+	if err != nil {
+		log.Fatal("Error starting server:", err)
+	}
+}
